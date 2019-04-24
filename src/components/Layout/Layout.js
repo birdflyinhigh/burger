cfa0a6416bbcf981d7f25751251daf1e7d9b7436
taojian <taojian@zbj.com>
2019-04-24 17:34:56 +0800

    implementation of burger builder container

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/axu';
 
+import classes from './Layout.css';
+
 const Layout = (props) => (
     <Aux>
         <div>Toolbar, SideDrawer, BackDrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import Aux from '../../hoc/axu';
+
+
+class BurgerBuilder extends Component {
+
+
+    render() {
+        return (
+            <Aux>
+                <div>Burger</div>
+                <div>Build Controls</div>
+            </Aux>
+        );
+    }
+
+
+}
+
+export default BurgerBuilder;
