47a5f151476a64678239bcd4698d3178cc4d84b7
taojian <taojian@zbj.com>
2019-07-22 17:19:39 +0800

    132 add tool bar in the navigation header

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ class App extends Component {
         return (
             <div>
                 <Layout>
-                    <p>LOOPINSIGHTS</p>
                     <BurgerBuilder/>
                 </Layout>
             </div>
---
+++ src/components/Navigation/ToolBar/ToolBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import classes from './ToolBar.css';
+
+const toolBar = (props)=> {
+    return (
+        <header className={classes.TooBar}>
+            <div>Menu</div>
+            <div>Logo</div>
+            <nav>
+                ...
+            </nav>
+
+
+        </header>
+    )
+}
+
+export default toolBar;
